cc3edbd8dadc82596875480de3ae97619dc79a5c
Jovan Gjorgjiev <joki.joksi@hotmail.com>
2022-11-24 20:53:04 +0100

    Complete site

--- G2/Class_10/incomplete-site/js/app.js
+++ G2/Class_10/incomplete-site/js/app.js
@@ -2,7 +2,7 @@ const menu = document.querySelector("#menu");
 
 menu.addEventListener("click", function (e) {
     const links = document.querySelector(".nav-links");
-    links.classList.toggle("show-nav-links");
+    links.classList.toggle("show-links");
 });
 
 const observer = new IntersectionObserver(function (entries) {
@@ -10,7 +10,7 @@ const observer = new IntersectionObserver(function (entries) {
         if (entry.isIntersecting) {
             entry.target.classList.add("slide-in-show");
         }
-    });
-});
+    })
+})
 
-document.querySelectorAll('.slide-in').forEach(x => observer.observe(x));
+document.querySelectorAll(".slide-in").forEach(entry => observer.observe(entry));
